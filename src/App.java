b401152be404917c4f3128cbbb5a980156cb7fd3
Diego Armando <diegoarmandoo@hotmail.com>
2022-05-02 03:16:30 -0300

    refactor(App): melhora o input/entrada dos dados
    Adiciona a entrada de dados através do teclado;
    Cria métodos para modularizar a adição de um ou vários alunos;
    Imprime uma lista de alunos.

--- src/App.java
+++ src/App.java
@@ -1,19 +1,88 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         
-        Aluno aluno01 = new Aluno(1234,"Diego Armando", "de Oliveira Meneses", 35, "Informática", "Sergipe", true);
-        Aluno aluno02 = new Aluno(3456,"Pedro José", "dos Santos", 28, "Comércio", "Riachão do Dantas", false);
+        //Permite a entrada de dados através do teclado
+        Scanner ler = new Scanner(System.in);
 
-        System.out.println(aluno01.toString());
-        System.out.println(aluno02.toString());
+        //Definir o atributo estático - Nome da Instituição
+        System.out.printf("Informe o nome da instituição de ensino: \n");
+        Aluno.setNomeInstituicao(ler.next());
 
-        System.out.println("Aluno 01 é igual a aluno 02: " + aluno01.equals(aluno02));
+        //Criar Alunos
+        ArrayList<Aluno> alunos = adicionarListaAlunos();
 
-        System.out.println("Quantidade de Alunos: " + Aluno.getQuantidadeAlunos());
+		//Imprimir os dados dos alunos
+		for (Aluno aluno: alunos) {
+			System.out.printf("------------------------------ \n");
+			System.out.printf("Aluno \n");
+			System.out.printf(aluno.toString());
+		}
 
-        Professor professor01 = new Professor(9999,"Maria Conceição", 48, 'F', "999.999.999-99", "Rua B - Bairro B");
-
-        System.out.println(professor01.toString());
+        System.out.println("Nome da Instituição: " + Aluno.getNomeInstituicao());
 
     }
+
+    public static Aluno adicionarAluno(){
+
+        //Permite a entrada de dados através do teclado
+		Scanner ler = new Scanner(System.in);
+
+        //Criar o aluno
+        Aluno aluno = new Aluno();
+
+        System.out.printf("Informe a matrícula do aluno: \n");
+		aluno.setMatricula(ler.nextInt());
+
+        System.out.printf("Informe a nome do aluno: \n");
+		aluno.setNome(ler.next());
+
+        System.out.printf("Informe o sobrenome do aluno: \n");
+		aluno.setSobrenome(ler.next());
+
+        System.out.printf("Informe a idade do aluno: \n");
+		aluno.setIdade(ler.nextInt());
+
+        System.out.printf("Informe o curso do aluno: \n");
+		aluno.setCurso(ler.next());
+
+        System.out.printf("Informe a naturalidade do aluno: \n");
+		aluno.setNaturalidade(ler.next());
+
+		System.out.printf("Informe se o aluno está matriculado: \n");
+		aluno.setEstaMatriculado(ler.nextBoolean());
+
+        return aluno;
+
+    }
+
+    public static ArrayList<Aluno> adicionarListaAlunos(){
+
+        //Permite a entrada de dados através do teclado
+		Scanner ler = new Scanner(System.in);
+
+        //Cria uma variável local alunos do Tipo ArrayList de Aluno
+        ArrayList<Aluno> alunos = new ArrayList<Aluno>();
+
+        System.out.printf("Informe a quantidade de alunos a serem cadastrados: \n");
+		int qtdAlunos = ler.nextInt();
+
+        //Estrtura de Repetição - Adicionar Alunos
+        for (int i = 1; i <= qtdAlunos; i++) {
+
+            System.out.printf("-------------------------------\n");
+			System.out.printf(i + "º Aluno \n");
+			System.out.printf("-------------------------------\n");
+
+            Aluno aluno = adicionarAluno();
+
+            alunos.add(aluno);
+            
+        }
+
+        return alunos;
+    }
+
 }
