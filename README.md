7445fc76e08c7cb2e9c27280a635aad65397f561
Diego Armando <diegoarmandoo@hotmail.com>
2022-05-01 21:07:52 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# academico-api
+ [API] Este é um repositório que armazena os códigos de um sistema acadêmico. Usado para ensinar os conceitos e fundamentos do Paradigma Orientado a Objetos.
---
+++ src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        int matricula = 1234;
+        String nome = "Diego Armando";
+        int idade = 35;
+        String curso = "Informática";
+        String naturalidade = "Sergipe";
+        boolean estaMatriculado = false;
+
+        System.out.println("Matricula: " + matricula);
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Curso: " + curso);
+        System.out.println("Naturalidade: " + naturalidade);
+        System.out.println("Está Matriculado? " + estaMatriculado);
+
+    }
+}
