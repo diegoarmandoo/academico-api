f15c1f26d926cfab97e267ab3fcd074c5c3baee7
Diego Armando <diegoarmandoo@hotmail.com>
2022-05-02 01:42:21 -0300

    refactor(Aluno): implementa novos conceitos do Paradigma Orientado a Objetos
    Adiciona os Gets e Sets;
    Cria o método equals;
    Adiciona atributos e métodos estáticos;
    Refatora o construtor e o método ToString.

--- src/Aluno.java
+++ src/Aluno.java
@@ -10,10 +10,99 @@ public class Aluno {
     
     int matricula;
     String nome;
+    String sobrenome;
     int idade;
     String curso;
     String naturalidade;
     boolean estaMatriculado;
+
+    //Atributo estático ou atributo da classe
+    static int quantidadeAlunos = 0;
+    static String nomeInstituicao = "IFS";
+
+    //Gets e Sets
+
+    /*
+		Os Gets e Sets são métodos usados para manipular os atributos da classe ou do objeto
+		São importantes para alcançar o conceito de encapsulamento
+		Cada atributo geralmente possui o seu respectivo get e set, onde o get é usado para recuperar o valor armazenado no atributo e o set usado para armazernar um valor no atributo
+	*/
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getNomeCompleto(){
+        return this.nome + " " + this.sobrenome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getNaturalidade() {
+        return naturalidade;
+    }
+
+    public void setNaturalidade(String naturalidade) {
+        this.naturalidade = naturalidade;
+    }
+
+    public boolean isEstaMatriculado() {
+        return estaMatriculado;
+    }
+
+    public void setEstaMatriculado(boolean estaMatriculado) {
+        this.estaMatriculado = estaMatriculado;
+    }
+
+    public static int getQuantidadeAlunos() {
+        return quantidadeAlunos;
+    }
+
+    public static void setQuantidadeAlunos(int quantidadeAlunos) {
+        Aluno.quantidadeAlunos = quantidadeAlunos;
+    }
+
+    public static String getNomeInstituicao() {
+        return nomeInstituicao;
+    }
+
+    public static void setNomeInstituicao(String nomeInstituicao) {
+        Aluno.nomeInstituicao = nomeInstituicao;
+    }
 
     //Construtores
 
@@ -24,15 +113,18 @@ public class Aluno {
 	*/
 
     public Aluno() {
+        this.incrementaQuantidadeAlunos();
     }
 
-    public Aluno(int matricula, String nome, int idade, String curso, String naturalidade, boolean estaMatriculado) {
+    public Aluno(int matricula, String nome, String sobrenome, int idade, String curso, String naturalidade, boolean estaMatriculado) {
         this.matricula = matricula;
         this.nome = nome;
+        this.sobrenome = sobrenome;
         this.idade = idade;
         this.curso = curso;
         this.naturalidade = naturalidade;
         this.estaMatriculado = estaMatriculado;
+        this.incrementaQuantidadeAlunos();
     }
 
     //Métodos
@@ -46,11 +138,30 @@ public class Aluno {
         String detalhes = "";
         detalhes += "Matrícula: " + this.matricula + " \n";
         detalhes += "Nome: " + this.nome + " \n";
+        detalhes += "Sobrenome: " + this.getSobrenome() + " \n";
+        detalhes += "Nome Completo: " + this.getNomeCompleto() + " \n";
         detalhes += "Idade: " + this.idade + " \n";
         detalhes += "Curso: " + this.curso + " \n";
         detalhes += "Naturalidade: " + this.naturalidade + " \n";
         detalhes += "Esta Matriculado? " + this.estaMatriculado + " \n";
+        detalhes += "Nome da Instituição " + Aluno.getNomeInstituicao() + " \n";
         return detalhes;
+    }
+
+    //O métodos equals() é uma forma de polimorfismo dinâmico, que chamamos de sopreposição, já que esse método é definido na classe object
+    //O Polimorfismo Dinâmico acontece na herança, quando a subclasse sobrepõe o método original.
+    @Override
+    public boolean equals(Object obj){
+        if (obj instanceof Aluno){
+            Aluno aluno = (Aluno) obj;
+            return this.matricula == aluno.getMatricula();
+        }
+        return false;
+    }
+
+    //Método que incrementa o atributo estático quantidadeAlunos 
+    private void incrementaQuantidadeAlunos(){
+        ++Aluno.quantidadeAlunos;
     }
 
 }
--- src/App.java
+++ src/App.java
@@ -1,11 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
-        Aluno aluno01 = new Aluno(1234,"Diego Armando", 35, "Informática", "Sergipe", true);
-        Aluno aluno02 = new Aluno(3456,"Pedro José", 28, "Comércio", "Riachão do Dantas", false);
+        Aluno aluno01 = new Aluno(1234,"Diego Armando", "de Oliveira Meneses", 35, "Informática", "Sergipe", true);
+        Aluno aluno02 = new Aluno(3456,"Pedro José", "dos Santos", 28, "Comércio", "Riachão do Dantas", false);
 
         System.out.println(aluno01.toString());
         System.out.println(aluno02.toString());
+
+        System.out.println("Aluno 01 é igual a aluno 02: " + aluno01.equals(aluno02));
+
+        System.out.println("Quantidade de Alunos: " + Aluno.getQuantidadeAlunos());
 
         Professor professor01 = new Professor(9999,"Maria Conceição", 48, 'F', "999.999.999-99", "Rua B - Bairro B");
 
