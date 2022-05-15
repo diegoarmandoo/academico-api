22d946bf20f1339137e2c2b0fc154738eb842cd8
Diego Armando <diegoarmandoo@hotmail.com>
2022-05-15 19:10:38 -0300

    refactor(Aluno): implementa os métodos concretos para gerar matrícula

--- src/Professor.java
+++ src/Professor.java
@@ -1,4 +1,21 @@
+import java.util.Random;
+
 public class Professor extends Pessoa {
     
+    public Professor() {
+        super();
+        this.setMatricula(this.gerarMatricula());
+    }
+
+    // O método gerarMatricula é uma sobrescrita
+	// A implementação do método gerarMatricula na classe Aluno sobrescreve o método abstrato definido na classe Pessoa
+	// Associaçaõ Comportamental - Dependência da Classe Aluno com as classes Random e Calendar através de imports.
+	@Override
+	public int gerarMatricula() {
+        Random gerador = new Random(); //Instancia a classe Random
+        int min = 10000000;  //Define o valor mínimo para do valor aletório
+	    int max = 99999999; //Define o valor máximo para do valor aletório
+        return gerador.nextInt(max - min + 1) + min;
+    }
 
 }
