3d45afa0c4a409d76cfb73ca2188f3acbf83ceef
Diego Armando <diegoarmandoo@hotmail.com>
2022-05-29 23:09:28 -0300

    refactor(Professor): implementa a funcionalidade de salário e carga horária
    - Adicionar os atributos e seus Gets e Sets
    - Refatora o ToString

--- src/Professor.java
+++ src/Professor.java
@@ -1,7 +1,32 @@
 import java.util.Random;
 
 public class Professor extends Pessoa {
+
+    // Atributos de Instância ou do Objeto
+		
+    double salario;
+    int cargaHoraria;
     
+    // Gets e Sets
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    // Construtor
+
     public Professor() {
         super();
         this.setMatricula(this.gerarMatricula());
@@ -16,6 +41,19 @@ public class Professor extends Pessoa {
         int min = 10000000;  //Define o valor mínimo para do valor aletório
 	    int max = 99999999; //Define o valor máximo para do valor aletório
         return gerador.nextInt(max - min + 1) + min;
+    }
+
+    // O métodos toString() é uma forma de polimorfismo dinâmico, que chamamos de
+    // sopreposição, já que esse método é definido na classe Pessoa
+    // O Polimorfismo Dinâmico acontece na herança, quando a subclasse sobrepõe o
+    // método original.
+    @Override
+    public String toString() {
+        String detalhes = "";
+        detalhes += super.toString();
+        detalhes += "Salário: " + this.getSalario() + " \n";
+        detalhes += "Carga Horária " + this.getCargaHoraria() + " \n";
+        return detalhes;
     }
 
 }
