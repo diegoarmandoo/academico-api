27d77a8e7d6bc76621ff4c2591b31ff74e8b1d2d
Diego Armando <diegoarmandoo@hotmail.com>
2022-06-04 03:49:52 -0300

    feat:(Disciplina): cria a classe que representa a disciplina
    - Adiciona os atributos
    - Adiciona os Gets e Sets
    - Adiciona o ToString
    - Adiciona o Construtor

---
+++ src/Disciplina.java
@@ -0,0 +1,42 @@
+public class Disciplina {
+
+    // Atributos de Instância ou do Objeto
+
+    String nome;
+    int cargaHoraria;
+
+    //Gets e Sets
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    // Construtores 
+
+    public Disciplina() {
+    }
+
+    //Métodos
+
+    @Override
+    public String toString() {
+        String detalhes = "";
+        detalhes += "Nome: " + this.getNome() + " \n";
+        detalhes += "Carga Horária " + this.getCargaHoraria() + " \n";
+        return detalhes;
+    } 
+    
+
+}
